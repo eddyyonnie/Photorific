c140f4b74a9ccb02b5a0ea89d0669444194cd855
eddy <eddyyonnie@gmail.com>
2019-05-15 20:02:53 +0300

    imported loc img photo in adminpy

--- terrific/admin.py
+++ terrific/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Location,Category,Photo
 
 admin.site.register(Location)
-admin.site.register(Category)
+admin.site.register(Category) 
 admin.site.register(Photo)
  
 # Register your models here.
